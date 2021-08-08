ba78fe74f376e77f187702059a557063b3d40382
David Malmström <syntex@outlook.com>
2021-08-08 22:56:24 +0200

    improve countSuccess function

--- src/functions/dice.functions.test.ts
+++ src/functions/dice.functions.test.ts
@@ -1,8 +1,25 @@
-import { isSuccess } from './dice.functions'
+import { countSuccesses } from './dice.functions'
 
-describe('getRandomInt', () => {
-  it.each([[1, false]])('%i => %b', (input, expected) => {
-    const result = isSuccess(input)
-    expect(result).toEqual(expected)
+describe('dice functions', () => {
+  describe('countSuccesses', () => {
+    it.each([
+      [1, 0],
+      [2, 0],
+      [3, 0],
+      [4, 0],
+      [5, 0],
+      [6, 1],
+      [7, 1],
+      [8, 2],
+      [9, 2],
+      [10, 3],
+      [11, 3],
+      [12, 4],
+      [13, 4],
+      [14, 5],
+    ])('%i => %b', (input, expected) => {
+      const result = countSuccesses(input)
+      expect(result).toEqual(expected)
+    })
   })
 })
--- src/functions/dice.functions.ts
+++ src/functions/dice.functions.ts
@@ -1,8 +1,8 @@
-export const isSuccess = (roll: number) => {
-  if (roll === -1) return false
-
-  return true
-}
+/**
+ * Can be used for any roll
+ */
+export const countSuccesses = (roll: number): number =>
+  roll < 6 ? 0 : Math.floor(roll / 2) - 2
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
 function getRandomInt(min = 1, max = 6) {
