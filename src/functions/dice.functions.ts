76ea513a8b4706ed0063af299951dfa655b189e2
David Malmström <syntex@outlook.com>
2021-08-29 03:16:51 +0200

    add choose function

--- src/functions/dice.functions.ts
+++ src/functions/dice.functions.ts
@@ -12,3 +12,6 @@ export function getRandomInt(min = 1, max = 6) {
   //The maximum is inclusive and the minimum is inclusive
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+export const choose = <T>(arr: readonly T[]): T =>
+  arr[getRandomInt(0, arr.length - 1)]
