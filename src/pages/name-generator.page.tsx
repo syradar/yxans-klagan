4a6e7928096047066e1c1359f4ad388bcabc94af
David Malmström <syntex@outlook.com>
2021-08-29 03:17:23 +0200

    add erländare name generator

--- src/pages/name-generator.page.tsx
+++ src/pages/name-generator.page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'twin.macro'
 import { Button } from '../components'
 import { Parchment } from '../components/parchment'
+import { range } from '../functions/array.functions'
+import { getRandomEländerName } from '../functions/name.functions'
 
 export const NameGeneratorPage = () => {
+  const getNames = (count = 10) =>
+    range(count).map((_) => getRandomEländerName())
+
+  const [erländerNames, setErländerNames] = useState<string[]>(getNames())
+
+  const getErländerNames = () => {
+    setErländerNames(getNames())
+  }
+
   return (
     <div tw="flex flex-col gap-y-8">
       <h1 tw="text-center text-6xl" className="yx-heading">
@@ -14,14 +25,17 @@ export const NameGeneratorPage = () => {
           Mäniskonamn
         </h2>
 
-        <Button
-          variant="primary"
-          onClick={() => {
-            console.log('name')
-          }}
-        >
-          Ge mig fler namn
+        <Button variant="primary" onClick={() => getErländerNames()}>
+          Erländare
         </Button>
+
+        {erländerNames.length > 0 && (
+          <ul>
+            {erländerNames.map((name, i) => (
+              <li key={i}>{name}</li>
+            ))}
+          </ul>
+        )}
       </Parchment>
     </div>
   )
