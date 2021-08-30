8a08c38a99743d23024ac10b4301c4c0841ed561
David Malmström <syntex@outlook.com>
2021-08-30 04:44:09 +0200

    add weighted names

--- src/functions/name.functions.ts
+++ src/functions/name.functions.ts
@@ -1,6 +1,45 @@
-import { humanNames } from '../data/name.data'
-import { choose } from './dice.functions'
+import { Gender } from '../models/general.model'
+import {
+  humanNames,
+  NameList,
+  NameType,
+  villageNames,
+} from '../models/name.model'
+import { choose, weightedRandom } from './dice.functions'
 
-export const getRandomEländerName = () => {
-  return choose(humanNames.Eländer.male)
+export const getRandomEländerName = (gender: Gender = Gender.Female) => {
+  const { type, firstName } = getNameTypeAndFirstName(
+    gender,
+    humanNames.Eländer,
+  )
+
+  switch (type) {
+    case NameType.FamilyName:
+      return `${firstName} ${choose(humanNames.Eländer.family)}`
+    case NameType.HomeName:
+      return `${firstName} av ${getRandomVillageName()}`
+    case NameType.FirstName:
+    default:
+      return firstName
+  }
+}
+
+export const getRandomVillageName = () =>
+  `${choose(villageNames.prefix)}${choose(villageNames.suffix)}`
+
+export const getNameTypeAndFirstName = (g: Gender, nl: NameList) => {
+  switch (g) {
+    case Gender.Male:
+      return {
+        type: weightedRandom(nl.male.probabilites).type,
+        firstName: choose(nl.male.rawNames),
+      }
+
+    case Gender.Female:
+    default:
+      return {
+        type: weightedRandom(nl.female.probabilites).type,
+        firstName: choose(nl.female.rawNames),
+      }
+  }
 }
--- src/pages/name-generator.page.tsx
+++ src/pages/name-generator.page.tsx
@@ -4,15 +4,22 @@ import { Button } from '../components'
 import { Parchment } from '../components/parchment'
 import { range } from '../functions/array.functions'
 import { getRandomEländerName } from '../functions/name.functions'
+import { Gender } from '../models/general.model'
 
 export const NameGeneratorPage = () => {
-  const getNames = (count = 10) =>
-    range(count).map((_) => getRandomEländerName())
+  const getNames = (count: number, gender: Gender) =>
+    range(count).map((_) => getRandomEländerName(gender))
 
-  const [erländerNames, setErländerNames] = useState<string[]>(getNames())
+  const [erländerNames, setErländerNames] = useState<string[]>(
+    getNames(10, Gender.Female),
+  )
+  const [maleErländerNames, setMaleErländerNames] = useState<string[]>(
+    getNames(10, Gender.Male),
+  )
 
   const getErländerNames = () => {
-    setErländerNames(getNames())
+    setErländerNames(getNames(10, Gender.Female))
+    setMaleErländerNames(getNames(10, Gender.Male))
   }
 
   return (
@@ -28,14 +35,35 @@ export const NameGeneratorPage = () => {
         <Button variant="primary" onClick={() => getErländerNames()}>
           Erländare
         </Button>
+        <div tw="grid grid-cols-2">
+          <div>
+            <h3 tw="text-2xl" className="yx-heading">
+              Kvinnor
+            </h3>
 
-        {erländerNames.length > 0 && (
-          <ul>
-            {erländerNames.map((name, i) => (
-              <li key={i}>{name}</li>
-            ))}
-          </ul>
-        )}
+            {erländerNames.length > 0 && (
+              <ul>
+                {erländerNames.map((name, i) => (
+                  <li key={i}>{name}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <div>
+            <h3 tw="text-2xl" className="yx-heading">
+              Män
+            </h3>
+
+            {maleErländerNames.length > 0 && (
+              <ul>
+                {maleErländerNames.map((name, i) => (
+                  <li key={i}>{name}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
       </Parchment>
     </div>
   )
