2915551b1691f8f5ce194f7cb6e1c6794306a8b0
David Malmström <syntex@outlook.com>
2021-10-17 05:00:01 +0200

    rename i18n to i18nReact

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { GlobalStyles } from './styles/GlobalStyles'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
 import { I18nextProvider } from 'react-i18next'
-import i18nReact from './i18n'
+import i18nReact from './i18nReact'
 
 ReactDOM.render(
   <React.StrictMode>
