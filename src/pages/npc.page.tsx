b5980363d64f968028f90c1d9a013f07b3abbba5
David Malmström <syntex@outlook.com>
2022-02-08 05:23:30 +0100

    feat: add npc characteristics to npc page

--- src/pages/npc.page.tsx
+++ src/pages/npc.page.tsx
@@ -5,65 +5,41 @@ import { Button } from '../components/Button'
 import { PageHeader } from '../components/page-header'
 import { Parchment } from '../components/parchment'
 import { Pancake, Train } from '../components/stack'
+import { getRandomCharacteristic } from '../functions/characteristics.function'
 import { getRandomOccupation } from '../functions/occupation.function'
-import { Occupation } from '../models/occupation.model'
-import { getId, ID } from '../models/utils.model'
-
-type OldOccupation = {
-  id: ID
-  occupation: Occupation
-}
+import { NPC } from '../models/npc.model'
 
 export const NpcPage = () => {
   const { t, i18n } = useTranslation(['npc', 'common'])
-  const [occupation, setOccupation] = useState<Occupation>(
-    getRandomOccupation(),
-  )
-  const [oldOccupation, setOldOccupation] = useState<OldOccupation[]>([])
+
+  const createNPC = (): NPC => ({
+    occupation: getRandomOccupation(),
+    characteristic: getRandomCharacteristic(),
+  })
+
+  const [npc, setNpc] = useState<NPC>(createNPC())
 
   const generateOccupation = () => {
-    setOldOccupation(() =>
-      [
-        {
-          occupation: occupation,
-          id: getId(),
-        },
-        ...oldOccupation,
-      ].slice(0, 5),
-    )
-    setOccupation(getRandomOccupation())
+    setNpc(createNPC())
   }
 
   return (
     <div tw="flex flex-col gap-y-8 w-full pb-16">
       <PageHeader>{t('Title')}</PageHeader>
-      <Parchment deps={[i18n.language, occupation, oldOccupation]}>
+      <Parchment deps={[i18n.language, npc]}>
         <Pancake>
-          <Train distribute>
-            <h2 tw="text-2xl lg:(text-4xl)" className="yx-heading">
-              {t('OccupationHeader')}
-            </h2>
+          <Train>
             <Button isSmall onClick={() => generateOccupation()}>
-              {t('OccupationButton')}
+              {t('NpcButton')}
             </Button>
           </Train>
 
           <div tw="text-lg lg:(text-2xl)" className="yx-prose">
-            {t(`Occupation.${occupation}`)}
+            {t(`Occupation.${npc.occupation}`)}
           </div>
-          <Pancake spacing="small">
-            {[...oldOccupation].map((oo, index) => (
-              <div
-                key={oo.id}
-                tw="text-gray-500"
-                css={{
-                  opacity: 1 - index * 0.2,
-                }}
-              >
-                {t(`Occupation.${oo.occupation}`)}
-              </div>
-            ))}
-          </Pancake>
+          <div tw="text-lg lg:(text-2xl)" className="yx-prose">
+            {t(`Characteristic.${npc.characteristic}`)}
+          </div>
         </Pancake>
       </Parchment>
     </div>
