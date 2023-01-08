700c69176f249b5d8aa3ff8d5bc49835fdfb9c72
David Malmström <syntex@outlook.com>
2023-01-08 11:30:03 +0100

    fix: remove core prefix from TranslationKey

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -13,13 +13,19 @@ export type TranslationKey<
   KEYS = FuncKey,
 > = GivenNS extends undefined
   ? KEYS extends `${infer NS}:${infer Key}`
-    ? `${NS}:${Key}`
+    ? NS extends 'core'
+      ? `${Key}`
+      : `${NS}:${Key}`
+    : never
+  : GivenNS extends 'core'
+  ? KEYS extends `${GivenNS}:${infer Key}`
+    ? `${Key}`
     : never
   : KEYS extends `${GivenNS}:${infer Key}`
   ? `${GivenNS}:${Key}`
   : never
 
-// const title1: TranslationKey = ''
-// const title1: TranslationKey<'core'> = ''
+// const title1: TranslationKey<'core'> = 'Menu-Calendar'
 // const title2: TranslationKey<'encounters'> = 'encounters:Title'
-// const title3: TranslationKey<'map' | 'core'> = ''
+// const title3: TranslationKey<'map' | 'core'> = 'Menu-Gear'
+// const title3: TranslationKey = 'Menucore:'
