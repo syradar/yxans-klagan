37f2bb1bc42ac4b733b67983ee848c1a5bfee0de
David Malmström <syntex@outlook.com>
2023-05-20 03:09:17 +0200

    fix: replace OR with nullish coalescing

--- src/features/calendar/calendar-slice.ts
+++ src/features/calendar/calendar-slice.ts
@@ -67,7 +67,7 @@ const calendarSlice = createSlice({
         [monthIndex]: {
           ...state.quarters[year]?.[monthIndex],
           [day]: spendQuarter(
-            state.quarters[year]?.[monthIndex]?.[day] || [
+            state.quarters[year]?.[monthIndex]?.[day] ?? [
               false,
               false,
               false,
