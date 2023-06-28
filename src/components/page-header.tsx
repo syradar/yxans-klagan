48bceae3bf3356ee75ec6862c737801d82af6463
David Malmström <syntex@outlook.com>
2023-06-28 05:40:35 +0200

    fix: make page headers smaller

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -2,5 +2,5 @@ type PageHeaderProps = {
   children?: React.ReactNode
 }
 export const PageHeader = ({ children }: PageHeaderProps) => (
-  <h1 className="yx-heading text-4xl text-amber-700 lg:text-6xl">{children}</h1>
+  <h1 className="yx-heading text-2xl text-amber-700 lg:text-4xl">{children}</h1>
 )
