417941028baf1c3f5aabc4293b444b97cf7e4867
David Malmström <syntex@outlook.com>
2023-07-03 18:51:52 +0200

    fix: fill BookList items by 2 columns

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -4,7 +4,14 @@ type BookListProps = ComponentPropsWithoutRef<'ul'>
 
 export const BookList = ({ children }: BookListProps) => {
   return (
-    <ul className="grid grid-cols-1 gap-x-4 gap-y-2 md:grid-cols-2">
+    <ul
+      className="grid grid-flow-col-dense grid-cols-1 gap-x-4 gap-y-2 md:grid-cols-2"
+      style={{
+        gridTemplateRows: Array.isArray(children)
+          ? `repeat(${Math.ceil(children.length / 2)}, minmax(0, 1fr))`
+          : undefined,
+      }}
+    >
       {children}
     </ul>
   )
